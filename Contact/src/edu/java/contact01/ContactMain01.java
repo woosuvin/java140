0013895d99ad8e6930b16053ec07c21a440e2315
woosuvin <dntnqls0605@gmail.com>
2023-03-17 18:08:44 +0900

    0317

--- Contact/src/edu/java/contact01/ContactMain01.java
+++ Contact/src/edu/java/contact01/ContactMain01.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class ContactMain01 {
 	// 상수(constant)
-	private static final int MAX_LENGTH = 2; // 연락처 배열의 최대 길이(원소 개수)
-	
+	private static final int MAX_LENGTH = 3; // 연락처 배열의 최대 길이(원소 개수)
+
 	// field
 	private Contact[] contacts = new Contact[MAX_LENGTH]; // 연락처를 저장할 배열.
 	private int count = 0; // 연락처 배열에 현재까지 저장된 연락처의 개수. 배열에 저장될 때마다 값 증가.
@@ -13,9 +13,9 @@ public class ContactMain01 {
 
 	public static void main(String[] args) {
 		System.out.println("***** 연락처 프로그램 v0.1 *****");
-		
+
 		ContactMain01 app = new ContactMain01(); // static이 아닌 메서드들을 사용하기 위해 참조변수 생성.
-		
+
 		boolean run = true;
 		while (run) {
 			// 메인 메뉴 보여주기
@@ -29,28 +29,46 @@ public class ContactMain01 {
 				app.insertNewContact();
 				break;
 			case 2: // 전체 목록
+				app.selectAllContact();
 				break;
 			case 3: // 배열의 인덱스로 검색
+//				app.searhContact();
+				app.selectContactByIndex();
 				break;
 			case 4: // 연락처 이름, 전화번호, 이메일 정보 수정
+				app.updateContactByIndex();
 				break;
 			case 5: // 배열의 인덱스로 연락처 삭제하기
+				app.deleteContactByIndex();
 				break;
 			default:
 				System.out.println(">메인 메뉴 번호를 확인하세요<");
 			}
 		}
-		
+
 		System.out.println("******** 프로그램 종료 ********");
-		
-		
-		
-	}//main end
-	
+
+	}// main end
+
+	public int showMainMenu() {
+		System.out.println();
+		System.out.println("----------------------------------------------------------");
+		System.out.println("[0]종료 [1]새 연락처 [2]전체 목록 [3]검색 [4]수정 [5]삭제");
+		System.out.println("----------------------------------------------------------");
+		System.out.print("선택 >>> ");
+
+		// 콘솔 창에서 입력된 문자열 1개 라인을 정수(int)로 변환.
+		int result = Integer.parseInt(scanner.nextLine()); // parseInt : 문자열을 int로 변환
+		return result;
+	}
+
+	// 1번
 	public void insertNewContact() {
 		if (count == MAX_LENGTH) {
-			System.out.println("!저장 불가!");
-		} else {
+			System.out.println("! 저장 공간 부족 !");
+			return; // 메서드 종료, 아래 코드 실행 안됨.
+		}
+
 		System.out.println();
 		System.out.println("--- 새 연락처 저장 ---");
 		System.out.print("이름 입력 >>> ");
@@ -59,35 +77,101 @@ public class ContactMain01 {
 		String phone = scanner.nextLine();
 		System.out.print("이메일 입력 >>> ");
 		String email = scanner.nextLine();
-		
+
 		// 입력받은 정보들로 Contact 타입의 객체 생성.
 		Contact c = new Contact(0, name, phone, email);
-		
+
 		// 생성된 Contact 타입의 객체를 배열에 저장.
 		contacts[count] = c;
-		
+
 		// 배열에 저장된 원소(연락처) 개수를 1 증가.
 		count++;
-		
-		System.out.println("!저장 성공!");
+
+		System.out.println("! 저장 성공 !");
+	}
+
+	// 2번
+	public void selectAllContact() {
+		System.out.println();
+		System.out.println("--- 연락처 목록 ---");
+//		for (count = 0; count < MAX_LENGTH; count++) {
+//			contacts[count].printInfo();
+		for (int i = 0; i < count; i++) {
+			contacts[i].printInfo();
 		}
 	}
-	
-	
-	
-	public int showMainMenu() {
+
+	// 3번
+//	public void searhContact() {
+//		System.out.print("검색 >>> ");
+//	    int i = scanner.nextInt();
+//	    contacts[i].printInfo();
+//	}
+	public void selectContactByIndex() {
 		System.out.println();
-		System.out.println("----------------------------------------------------------");
-		System.out.println("[0]종료 [1]새 연락처 [2]전체 목록 [3]검색 [4]수정 [5]삭제");
-		System.out.println("----------------------------------------------------------");
-		System.out.print("선택 >>> ");
-		
-		
-		// 콘솔 창에서 입력된 문자열 1개 라인을 정수(int)로 변환.
-		int result = Integer.parseInt(scanner.nextLine()); // parseInt : 문자열을 int로 변환
-		
-		return result;
+		System.out.println("--- 인덱스 검색 ---");
+		System.out.print("검색할 인덱스 입력 >>> ");
+		int index = Integer.parseInt(scanner.nextLine());
+
+		if (index >= 0 && index < count) {
+			contacts[index].printInfo();
+			
+		} else {
+		System.out.println("! 해당 인덱스에는 연락처 정보가 없습니다 !");
+		}
 	}
-	
+
+	// 4번
+	public void updateContactByIndex() {
+			System.out.println();
+			System.out.println("--- 연락처 수정 ---");
+			System.out.print("수정할 연락처 인덱스 입력 >>> ");
+			int index = Integer.parseInt(scanner.nextLine());
+			
+			if (index < 0 || index >= count) {
+				System.out.println("! 해당 인덱스에는 연락처 정보가 없습니다 !");
+				return;
+			}
+			System.out.print("수정 전: ");
+			contacts[index].printInfo(); // 수정 전 연락처 출력
+
+			System.out.print("수정할 이름 입력 >>> ");
+			String name = scanner.nextLine();
+			System.out.print("수정할 전화번호 입력 >>> ");
+			String phone = scanner.nextLine();
+			System.out.print("수정할 이메일 입력 >>> ");
+			String email = scanner.nextLine();
+
+			// 수정할 인덱스의 정보 업데이트
+			contacts[index].setName(name);
+			contacts[index].setPhone(phone);
+			contacts[index].setEmail(email);
+//		contacts[index] = new Contact(0, name, phone, email);
+			
+			System.out.print("수정 후: ");
+			contacts[index].printInfo();	
+	}
+
+	// 5번
+	public void deleteContactByIndex() {
+		
+			System.out.println();
+			System.out.println("--- 연락처 삭제 ---");
+			System.out.print("삭제할 연락처 인덱스 입력 >>> ");
+			int index = Integer.parseInt(scanner.nextLine());
+			
+			if (index < 0 || index >= count ) {
+				System.out.println("! 해당 인덱스에는 연락처 정보가 없습니다 !");
+				return;
+			}
+			
+			for (int i = index; i < count - 1; i++) {
+				contacts[i] = contacts[i + 1]; // 뒷쪽 연락처 정보를 한칸 앞으로
+			}
+			contacts[count - 1] = null; // 삭제 전 배열의 마지막 원소를 null;
+			count--; // 배열 원소의 개수를 1 줄임.
+
+			System.out.println("! 삭제 성공 !");		
+	}
 
 }
---
+++ Java07_inheritance/src/edu/java/inheritance02/InheritanceMain02.java
@@ -0,0 +1,17 @@
+package edu.java.inheritance02;
+
+public class InheritanceMain02 {
+
+	public static void main(String[] args) {
+		// 상속 관계에서 생성자 호출 순서:
+		BusinessPerson p1 = new BusinessPerson();
+		BusinessPerson p2 = new BusinessPerson("아이티윌");
+		BusinessPerson p3 = new BusinessPerson("우수빈", "아이티윌");
+		
+		
+		
+		
+		
+	}
+
+}
